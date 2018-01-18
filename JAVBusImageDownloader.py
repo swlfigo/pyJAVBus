cb087e269ede985e3555aa4cc1048d557ba46a2b
你见唔到我 <403482009@qq.com>
2018-01-18 15:58:28 +0800

    【Feature】添加多线程下载

--- JAVBusImageDownloader.py
+++ JAVBusImageDownloader.py
@@ -2,10 +2,12 @@
 
 import requests
 import os
+import queue
 import sys
 from javbus.Utils import syFileOperator
 from javbus.Utils import syLogger
 from javbus.Utils import JAVBusMySQLDBManager
+from javbus.Utils import JAVBusImageDownloadQueue
 
 
 
@@ -19,17 +21,23 @@ class JAVImageDownloader():
         self.logger = syLogger.syLoggerManager()
         # 创建资源目录
         #  PYJAVBUS ...  (项目文件根)javbus (同级) /javbus / Utils / currentPath
-        self.sourcePath = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(self.fileOperator.currentPath))),
-                                       'PYJAVBUS')
+        self.sourcePath = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(self.fileOperator.currentPath))),'PYJAVBUS')
         if self.fileOperator.isExistsFilePath(self.sourcePath) == False:
             self.fileOperator.createDirPath(self.sourcePath)
+        #Queue
+        self.downloadImageQueue = queue.Queue()
+        #Resource
+        self.downloadSource = []
+
 
     def startDownLoadImage(self):
         print('SourceDownloadPath:%s'%self.sourcePath)
         sqlString = '''SELECT * FROM %s'''%self.dbManager.javTableName
         items = self.dbManager.executeWithDictReturn(sqlString)
         if len(sqlString) == 0 or sqlString is None:
-            self.logger.syLog('无下载资源图片')
+            self.logger.syLog('无下载资源')
+            return
+
         for item in items:
             if len(item['samplePic']) == 0 or item['samplePic'] is None or len(item['cover']) == 0:
                 self.logger.syLog('无样例图片')
@@ -38,47 +46,42 @@ class JAVImageDownloader():
             rootPath = os.path.join(self.sourcePath, rootPath)
             if self.fileOperator.isExistsFilePath(rootPath) == False:
                 self.fileOperator.createDirPath(rootPath)
+            # 样品图
             samplePic = item['samplePic']
-            samplePicArray = samplePic.split('||')
-            #样品图
-            for picURLAddress in samplePicArray:
-                list_name = picURLAddress.split('/')
-                # 图片名称
-                file_name = str(list_name[len(list_name) - 1])
-                filePath = os.path.join(rootPath, file_name)
-                #存在图片则不用下载
-                if self.fileOperator.isExistsFilePath(filePath) == True:
-                    self.logger.syLog('存在 %s 样品图 %s'%(str(item['code']) , file_name))
-                    continue
-                try:
-                    ir = requests.get(picURLAddress, timeout=2)
-                except Exception as e:
-                    self.logger.syLog('下载 %s 样品图 %s 错误 Error : %s '%(str(item['code']) , file_name , str(e)))
-                    continue
-                if ir.status_code == 200:
-                    with open(filePath, 'wb') as f:
-                        f.write(ir.content)
-                        f.close()
-                        self.logger.syLog('下载 %s 样品图 %s 成功' % (str(item['code']), file_name))
+            #如果没有图片则不用添加
+            if samplePic is not None and len(samplePic) > 0:
+                samplePicArray = samplePic.split('||')
+                for picURLAddress in samplePicArray:
+                    downInfoDict = {
 
-            #封面图
-            coverURL = item['cover']
-            coverSplit = coverURL.split('/')
-            cover_name = str(coverSplit[len(coverSplit) - 1])
-            coverPath = os.path.join(rootPath, cover_name)
-            if self.fileOperator.isExistsFilePath(coverPath) == True:
-                self.logger.syLog('存在 %s 封面 %s' % (str(item['code']), cover_name))
-                continue
-            try:
-                ir = requests.get(coverURL, timeout=2)
-            except Exception as e:
-                self.logger.syLog('下载 %s 封面 %s 错误 Error : %s ' % (str(item['code']), cover_name, str(e)))
-                return
-            if ir.status_code == 200:
-                with open(coverPath, 'wb') as f:
-                    f.write(ir.content)
-                    f.close()
-                    self.logger.syLog('下载 %s 封面 %s' % (str(item['code']), cover_name))
+                        "url":str(picURLAddress),
+                        "code":item['code'],
+                        "type":"samplePic"
+
+                    }
+                    self.downloadSource.append(downInfoDict)
+
+            if item['cover'] is not None :
+                downInfoDict = {
+
+                    "url":item['cover'],
+                    "code":item['code'],
+                    "type":"cover"
+
+                }
+                self.downloadSource.append(downInfoDict)
+
+        #多线程下载
+        for i in range(10):
+            queue = JAVBusImageDownloadQueue.javBusImageDownloadQueue(self.downloadImageQueue)
+            queue.setDaemon(True)
+            queue.start()
+        for item in downInfoDict:
+            self.downloadImageQueue.put(item)
+        self.downloadImageQueue.join()
+
+
+
 
 
 if __name__ == '__main__':
---
+++ javbus/Utils/JAVBusImageDownloadQueue.py
@@ -0,0 +1,70 @@
+# -*- coding:utf-8 -*-
+import queue
+import threading
+import requests
+from javbus.Utils import syLogger
+from javbus.Utils import syFileOperator
+import os
+
+class javBusImageDownloadQueue(threading.Thread):
+    def __init__(self,queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.downloadLock = threading.Lock()
+        self.fileOperator = syFileOperator.syFileOperator()
+        self.logger = syLogger.syLoggerManager()
+        # 资源目录
+        self.sourcePath = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(self.fileOperator.currentPath))),'PYJAVBUS')
+
+
+
+    def run(self):
+        while True:
+            item = self.queue.get()
+            rootPath = item['code']
+            rootPath = os.path.join(self.sourcePath, rootPath)
+            if self.fileOperator.isExistsFilePath(rootPath) == False:
+                self.fileOperator.createDirPath(rootPath)
+            url = item['url']
+
+            #封面图
+            if item['type'] == 'cover':
+                coverURL = url
+                coverSplit = coverURL.split('/')
+                cover_name = str(coverSplit[len(coverSplit) - 1])
+                coverPath = os.path.join(rootPath, cover_name)
+                if self.fileOperator.isExistsFilePath(coverPath) == True:
+                    self.logger.syLog('存在 %s 封面 %s' % (str(item['code']), cover_name))
+                    break
+                try:
+                    ir = requests.get(coverURL, timeout=2)
+                except Exception as e:
+                    self.logger.syLog('下载 %s 封面 %s 错误 Error : %s ' % (str(item['code']), cover_name, str(e)))
+                    break
+                if ir.status_code == 200:
+                    with open(coverPath, 'wb') as f:
+                        f.write(ir.content)
+                        f.close()
+                        self.logger.syLog('下载 %s 封面 %s' % (str(item['code']), cover_name))
+
+
+            #样例图片下载
+            elif item['type'] == 'samplePic':
+                list_name = url.split('/')
+                # 图片名称
+                file_name = str(list_name[len(list_name) - 1])
+                filePath = os.path.join(rootPath, file_name)
+                # 存在图片则不用下载
+                if self.fileOperator.isExistsFilePath(filePath) == True:
+                    self.logger.syLog('存在 %s 样品图 %s' % (str(item['code']), file_name))
+                    break
+                try:
+                    ir = requests.get(url, timeout=2)
+                except Exception as e:
+                    self.logger.syLog('下载 %s 样品图 %s 错误 Error : %s ' % (str(item['code']), file_name, str(e)))
+                    break
+                if ir.status_code == 200:
+                    with open(filePath, 'wb') as f:
+                        f.write(ir.content)
+                        f.close()
+                        self.logger.syLog('下载 %s 样品图 %s 成功' % (str(item['code']), file_name))
