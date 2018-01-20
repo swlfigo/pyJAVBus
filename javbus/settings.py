ee5f3073311837501c1a903068e50b57a8ed6a1b
你见唔到我 <403482009@qq.com>
2018-01-20 15:37:58 +0800

    no message

--- javbus/settings.py
+++ javbus/settings.py
@@ -26,6 +26,7 @@ ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
+CONCURRENT_ITEMS = 60
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
